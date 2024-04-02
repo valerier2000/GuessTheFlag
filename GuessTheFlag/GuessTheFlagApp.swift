4c26fcd459420e2ba2ca4f0c9083c8c10012ff53
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-02 09:09:39 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Valeriia Rohatynska on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
